8f9e6e02ac09ab52784ff8c3347243aaf98f1d65
Kevin Nguyen <ngynkvn@gmail.com>
2021-07-26 16:14:06 -0400

    idk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod frontend;
 use std::collections::HashMap;
 
 use std::io::{self, Stdout};
@@ -7,7 +8,7 @@ use std::time::Duration;
 
 use crossterm::event::{poll, read, Event, KeyCode, KeyEvent};
 use mqttrs::{decode_slice, encode_slice, Connack, Packet, Publish};
-use tokio::io::BufReader;
+
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream, ToSocketAddrs};
 use tokio::sync::mpsc::{channel, Receiver, Sender};
@@ -19,7 +20,7 @@ use tui::widgets::{Block, BorderType, Borders, List, ListItem, ListState, Paragr
 use tui::Terminal;
 
 #[derive(Debug)]
-struct Session {
+pub struct Session {
     addr: SocketAddr,
     is_on: bool,
     tx: Sender<[u8; 1024]>,
@@ -47,7 +48,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let stdout = io::stdout();
         let backend = CrosstermBackend::new(stdout);
         let terminal = Terminal::new(backend).unwrap();
-        tui_worker(state, terminal).await;
+        frontend::tui_worker(state, terminal).await;
     });
 
     let state = state_ref.clone();
@@ -59,108 +60,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         session.insert(addr, Session::new(addr, tx));
 
         let state = state.clone();
-        tokio::spawn(async move {
-            client_worker(socket, rx).await;
-        });
-    }
-}
-
-async fn tui_worker(
-    state: Arc<Mutex<HashMap<SocketAddr, Session>>>,
-    mut terminal: Terminal<CrosstermBackend<Stdout>>,
-) -> () {
-    terminal.clear().unwrap();
-    let mut list_state = ListState::default();
-    list_state.select(Some(0));
-    loop {
-        let store = state.lock().await;
-        terminal
-            .draw(|rect| {
-                let size = rect.size();
-                let chunks = Layout::default()
-                    .direction(Direction::Vertical)
-                    .margin(1)
-                    .constraints(
-                        [
-                            Constraint::Min(5),
-                            Constraint::Length(3),
-                            Constraint::Length(4),
-                        ]
-                        .as_ref(),
-                    )
-                    .split(size);
-                let items: Vec<_> = store
-                    .iter()
-                    .map(|(key, session)| ListItem::new(format!("{} ➡ {:?}", key, session)))
-                    .collect();
-                let list = List::new(items)
-                    .block(
-                        Block::default()
-                            .borders(Borders::ALL)
-                            .style(Style::default().fg(Color::White))
-                            .border_type(BorderType::Thick),
-                    )
-                    .highlight_style(
-                        Style::default()
-                            .bg(Color::Yellow)
-                            .fg(Color::Black)
-                            .add_modifier(Modifier::BOLD),
-                    );
-                rect.render_stateful_widget(list, chunks[0], &mut list_state)
-            })
-            .expect("???");
-
-        match poll(Duration::from_millis(200)) {
-            Ok(true) => {
-                if let Ok(Event::Key(event)) = read() {
-                    match event {
-                        KeyEvent {
-                            code: KeyCode::Down,
-                            ..
-                        } => {
-                            if let Some(index) = list_state.selected() {
-                                let len = store.len();
-                                let i = Some(if index >= len - 1 { 0 } else { index + 1 });
-                                list_state.select(i);
-                            }
-                        }
-                        KeyEvent {
-                            code: KeyCode::Up, ..
-                        } => {
-                            if let Some(index) = list_state.selected() {
-                                let len = store.len();
-                                let i = Some(if index > 0 { index - 1 } else { len - 1 });
-                                list_state.select(i);
-                            }
-                        }
-                        KeyEvent {
-                            code: KeyCode::Char('t'),
-                            ..
-                        } => {
-                            if let Some((id, session)) =
-                                store.iter().nth(list_state.selected().unwrap())
-                            {
-                                let mut buf = [0u8; 1024];
-                                let topic = format!("cmnd/{}/POWER", id);
-                                let command = mqttrs::Packet::Publish(Publish {
-                                    dup: false,
-                                    qospid: mqttrs::QosPid::AtMostOnce,
-                                    retain: false,
-                                    topic_name: &topic,
-                                    payload: "ON".as_bytes(),
-                                });
-                                encode_slice(&command, &mut buf);
-                                session.tx.send(buf).await;
-                            }
-                        }
-                        _ => {}
-                    }
-                }
-            }
-            Ok(false) => {}
-            Err(err) => panic!(err),
-        }
-        std::thread::sleep(std::time::Duration::from_millis(200));
+        println!("found conn");
+        // tokio::spawn(async move {
+        //     client_worker(socket, rx).await;
+        // });
     }
 }
 
@@ -173,50 +76,50 @@ async fn reply<'a>(socket: &mut tokio::net::TcpStream, packet: &Packet<'a>, buf:
     }
 }
 
-async fn client_worker(mut socket: TcpStream, mut rx: Receiver<([u8; 1024], usize)>) {
-    let mut buf = [0u8; 1024];
-    let (mut rd, mut wrt) = socket.split();
+// async fn client_worker(mut socket: TcpStream, mut rx: Receiver<([u8; 1024], usize)>) {
+//     let mut buf = [0u8; 1024];
+//     let (mut rd, mut wrt) = socket.split();
 
-    tokio::spawn(async move {
-        loop {
-            if let Some((buf, size)) = rx.recv().await {
-                wrt.write(&buf[..size]).await;
-            }
-        }
-    });
+//     tokio::spawn(async move {
+//         loop {
+//             if let Some((buf, size)) = rx.recv().await {
+//                 wrt.write(&buf[..size]).await;
+//             }
+//         }
+//     });
 
-    loop {
-        rd.read(&mut buf).await.unwrap();
-        match decode_slice(&buf) {
-            Ok(Some(pkt)) => match pkt {
-                mqttrs::Packet::Connect(pkt) => {
-                    let connack = Packet::Connack(Connack {
-                        session_present: false,
-                        code: mqttrs::ConnectReturnCode::Accepted,
-                    });
-                    reply(&mut socket, &connack, &mut buf).await;
-                }
-                Packet::Publish(pkt) => {
-                    // println!(
-                    //     "[{}]: {} [{:?}]",
-                    //     pkt.topic_name,
-                    //     String::from_utf8_lossy(&pkt.payload),
-                    //     pkt.payload,
-                    // )
-                }
-                Packet::Pingreq => {
-                    reply(&mut socket, &Packet::Pingresp, &mut buf).await;
-                }
-                unknown => {
-                    println!("Not handled: {:?}", unknown);
-                }
-            },
-            Ok(None) => {
-                println!("Decode error");
-            }
-            Err(error) => {
-                dbg!(error);
-            }
-        }
-    }
-}
+//     loop {
+//         rd.read(&mut buf).await.unwrap();
+//         match decode_slice(&buf) {
+//             Ok(Some(pkt)) => match pkt {
+//                 mqttrs::Packet::Connect(pkt) => {
+//                     let connack = Packet::Connack(Connack {
+//                         session_present: false,
+//                         code: mqttrs::ConnectReturnCode::Accepted,
+//                     });
+//                     reply(&mut socket, &connack, &mut buf).await;
+//                 }
+//                 Packet::Publish(pkt) => {
+//                     // println!(
+//                     //     "[{}]: {} [{:?}]",
+//                     //     pkt.topic_name,
+//                     //     String::from_utf8_lossy(&pkt.payload),
+//                     //     pkt.payload,
+//                     // )
+//                 }
+//                 Packet::Pingreq => {
+//                     reply(&mut socket, &Packet::Pingresp, &mut buf).await;
+//                 }
+//                 unknown => {
+//                     println!("Not handled: {:?}", unknown);
+//                 }
+//             },
+//             Ok(None) => {
+//                 println!("Decode error");
+//             }
+//             Err(error) => {
+//                 dbg!(error);
+//             }
+//         }
+//     }
+// }
